9bfce004e2aeb1f8adcb3327982dafb3b3f6b33e
Jacky Sio <jackysio.24@gmail.com>
2023-03-10 13:02:40 -0800

    updating models and scraper script

--- backend/ufc_scraper/models.py
+++ backend/ufc_scraper/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Fighter(models.Model):
@@ -8,24 +9,24 @@ class Fighter(models.Model):
     weight = models.CharField(max_length=10)
     reach = models.CharField(max_length=10)
     stance = models.CharField(max_length=20)
-    dob = models.DateField()
+    dob = models.DateField(default=datetime.date.today)
     def __str__(self):
         return self.name
 
 class Event(models.Model):
     fighter = models.ForeignKey(Fighter, on_delete=models.CASCADE)
     opponent = models.CharField(max_length=255)
-    result = models.CharField(max_length=10)
-    kd1 = models.IntegerField()
-    kd2 = models.IntegerField()
-    str1 = models.IntegerField()
-    str2 = models.IntegerField()
-    td1 = models.IntegerField()
-    td2 = models.IntegerField()
-    sub1 = models.IntegerField()
-    sub2 = models.IntegerField()
+    result = models.CharField(max_length=5)
+    kd1 = models.IntegerField(default=0, blank=True, null=True)
+    kd2 = models.IntegerField(default=0, blank=True, null=True)
+    str1 = models.IntegerField(default=0, blank=True, null=True)
+    str2 = models.IntegerField(default=0, blank=True, null=True)
+    td1 = models.IntegerField(default=0, blank=True, null=True)
+    td2 = models.IntegerField(default=0, blank=True, null=True)
+    sub1 = models.IntegerField(default=0, blank=True, null=True)
+    sub2 = models.IntegerField(default=0, blank=True, null=True)
     event = models.CharField(max_length=255)
-    date = models.DateField()
+    date = models.DateField(default=datetime.date.today)
     method = models.CharField(max_length=10)
     move = models.CharField(max_length=30)
     round = models.IntegerField()
